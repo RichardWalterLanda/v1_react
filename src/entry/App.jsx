00f5a50fdf918da1984b82058a3324d917d6a695
Johannes Richard Landa <142742200+RichardWalterLanda@users.noreply.github.com>
2023-09-15 01:19:48 +0200

    added home page and some nav items

---
+++ src/entry/App.jsx
@@ -0,0 +1,17 @@
+
+import { HomePage } from "../pages"
+import { MainWrapper} from "../shared"
+
+function App() {
+
+  return (
+    <div className="w-full h-full">
+      <MainWrapper>
+        <HomePage />
+      </MainWrapper>
+    </div>
+  )
+}
+
+export default App
+  
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,49 @@
+import { Outlet } from "react-router-dom";
+
+import { AboutInfo, GitHub, LeetCode, Navigation, Portfolio } from "../../shared";
+import { Route, Routes } from "react-router-dom";
+import { ContactForm } from "../../shared";
+
+export default function home() {
+
+  const menuItems = [
+    {
+      to: "/cv",
+      unit: "Portfolio"
+    },
+    {
+      to: "/contact",
+      unit: "Contact"
+    },
+    {
+      to: "/github",
+      unit: "GitHub"
+    },
+    {
+      to: "/leetcode",
+      unit: "LeetCode"
+    }
+  ]
+
+  return (
+    <div className="fullflex w-[1200px] h-[650px] p-4 custom-shadow rounded-2xl">
+      <div className="w-full h-full fullflex border-black shadow-2xl rounded-2xl">
+        <img src="../../../img/foto2.jpg" alt="Your Image" className="relative inset-0 w-full h-full object-cover object-right rounded-2xl" />
+      </div>
+      <div className="w-full h-full flex justify-start items-end flex-col ml-5">
+        <Navigation items={menuItems}></Navigation>
+        <div className="fullflex w-full h-full my-10">
+          <Routes>
+            <Route path="/">
+              <Route path="/contact" element={<ContactForm />} />
+              <Route path="/cv" element={<Portfolio />} />
+              <Route path="/github" element={<GitHub />}/>
+              <Route path="/leetcode" element={<LeetCode />} />
+            </Route>
+          </Routes>
+        </div>
+        <AboutInfo />
+      </div>
+    </div>
+  )
+}
